c7fe7c6b4c74f67d0ca03d9c8f685a873f9e97e7
alice.petiot <alice.petiot@outlook.fr>
2022-06-23 11:48:10 +0200

    tests

---
+++ tests/main_test.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('../') # allows us to fetch files from the project root
+
+import unittest
+from modules.main import *
+
+class InitTest(unittest.TestCase):
+
+class BoolCircTest(unittest.TestCase):
